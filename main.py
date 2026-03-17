f7ecb94673cf05db78c86765e132faafdee08153
Brett Kaplan <BrettCKaplan@gmail.com>
2026-03-16 21:17:18 -0400

    basic validation

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 
+import argparse
+import os
 import sys
 
 
-def main():
-    pass
+def main(dir: string):
+    print(dir)
+
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dir')
+    args = parser.parse_args()
+    dir = args.dir or os.getcwd()
+    if not os.path.exists(dir):
+        raise ValueError('dir passed in must exist')
     try:
-        main()
+        main(dir)
     except KeyboardInterrupt:
         pass
 sys.exit()
