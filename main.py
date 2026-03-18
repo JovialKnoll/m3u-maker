e0f35880b052dd75352624270dbbf6d6805dd68a
Brett Kaplan <BrettCKaplan@gmail.com>
2026-03-17 22:20:55 -0400

    directory

--- main.py
+++ main.py
@@ -16,28 +16,28 @@ def get_music_files(files):
     ]
 
 
-def make_m3u(dir: string):
+def make_m3u(directory: string):
     # todo: make and save m3u file
     # check against track number of files to ensure order is right
     files = [
         f
-        for f in os.listdir(dir)
-        if os.path.isfile(os.path.join(dir, f))
+        for f in os.listdir(directory)
+        if os.path.isfile(os.path.join(directory, f))
     ]
     music_files = get_music_files(files)
     if not music_files:
         print("no music files here, exiting")
         return
     music_files.sort()
-    m3u_file = os.path.join(dir, os.path.basename(dir) + '.m3u8')
+    m3u_file = os.path.join(directory, os.path.basename(directory) + '.m3u8')
     print(m3u_file)
     with open(m3u_file, 'w', encoding='utf-8') as f:
         for music_file in music_files:
             print(music_file, file=f)
 
 
-def main(dir: string):
-    for root, dirs, files in os.walk(dir):
+def main(directory: string, force):
+    for root, dirs, files in os.walk(directory):
         # skip if .m3u already exists
         if any(f.endswith('.m3u8') for f in files):
             continue
@@ -62,14 +62,17 @@ def main(dir: string):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--single', action='store_true', help="just run on this directory")
+    parser.add_argument('-f', '--force', action='store_true', help="replace existing .m3u8 files")
     parser.add_argument('dir', help="the directory to run on")
     args = parser.parse_args()
-    dir = args.dir.rstrip('"\\/')
-    if not os.path.exists(dir):
+    directory = args.dir.rstrip('"\\/')
+    if not os.path.exists(directory):
         raise ValueError("directory passed in must exist")
-    f = make_m3u if args.single else main
     try:
-        f(dir)
+        if args.single:
+            make_m3u(directory)
+        else:
+            main(directory, args.force)
     except KeyboardInterrupt:
         pass
     sys.exit()
